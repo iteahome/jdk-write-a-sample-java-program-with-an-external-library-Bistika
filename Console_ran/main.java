a3a24df238b7a85efde5f3fb33df80f3c8a469c6
Bistika <48261571+Bistika@users.noreply.github.com>
2019-12-03 15:06:10 +0200

    Update by removing final variables added by VScode. Do not need final variable.

--- Console_ran/main.java
+++ Console_ran/main.java
@@ -3,13 +3,13 @@ import org.joda.time.Duration;
 
 public class main {
 
-    public static void main(final String[] args) {
-        final DateTime now = DateTime.now();
+    public static void main(String[] args) {
+        DateTime now = DateTime.now();
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
-        final DateTime other = DateTime.now();
-        final Duration duration = new Duration(now, other);
+        DateTime other = DateTime.now();
+        Duration duration = new Duration(now, other);
         System.out.println(duration);
         System.out.println("Short difference of time, just to demonstrate.");
     }
